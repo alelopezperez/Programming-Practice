ef5ef18b66dd5714d83401aea08b4a692f1638fc
ale <Ale>
2019-01-24 23:36:14 -0600

    source link of the problem

--- Easy/plus-one-array.cpp
+++ Easy/plus-one-array.cpp
@@ -2,7 +2,7 @@
 //https://leetcode.com/problems/plus-one/
 
 // Time Complexity O(2N) equivalent to O(n) since we first need to pass one time through the array and a second time when adding the extra 1
-// Space Complexity O(1) constant.
+// Space Complexity O(1) constant
 vector<int> plusOne(vector<int>& digits) {
         
 	int i = digits.size()-1;
