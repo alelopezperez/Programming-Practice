e44c581054611436f789bd6a3c8561078a27b3b0
ale <Ale>
2019-02-02 22:13:57 -0600

    Implment a stack that returns also the min value in the stack in constant time; this uses alot of memory

---
+++ Easy/min-stack-using-aux-stack.cpp
@@ -0,0 +1,41 @@
+// I can use extra stack it does not matter; but also i need to stop looking at the looking quickly at the correct answers and believe in myself;
+//Time Complexity O(N);
+//Space Comlexity O(2*N) Since I use a main stack and one aux stack.
+//Time to completion 18min
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> st;
+    stack<int> min;
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        st.push(x);
+        if(min.empty() || min.top()>=x){
+            min.push(x);
+           
+        }
+        
+    }
+    
+    void pop() {
+        if(st.top()==min.top()){
+            min.pop();
+        }
+        st.pop();      
+        
+        
+    }
+    
+    int top() {
+        return st.top();
+        
+    }
+    
+    int getMin() {
+       
+        return min.top();
+    }
+};
