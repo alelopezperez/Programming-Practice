9a280b6c65337983707201786e7711dad85a5bc6
ale <malejlopez@hotmail.com>
2019-02-13 23:29:15 -0600

    N-ary Tree Preorder Traversal with a stack

--- Easy/n-ary-tree-preorder-traversal-with-recursion.cpp
+++ Easy/n-ary-tree-preorder-traversal-with-recursion.cpp
@@ -1,5 +1,5 @@
 //Time Complexity O(N) since we visit or traverse the complete tree
-//Space Complexity O(N) since we use the 
+//Space Complexity O(N) since we use recursion and we need to keep in my the recursion stack
 //time to completion 10min
 // I think i learned to finally do preorder with recursion it's easy first you look
 //then check it's child and keep doing it like that
