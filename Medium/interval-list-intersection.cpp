0cc0148fc9c4b9e2907b5a05484c9a94a210a208
ale <malejlopez@hotmail.com>
2020-03-20 21:44:17 -0600

    Learning Experience BF Interval List Intersection

---
+++ Medium/interval-list-intersection.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/interval-list-intersections/
+//Time Complexity O(N+M) 
+//Space Complexity O(1)
+// Check the ranges the one with the longest range move one
+// Check the interval as I did.
+//Time To Completion Failed BF
+
+
+vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
+        
+        
+        int i = 0;
+        int j = 0;
+        vector<vector<int>> ans;
+        while(i<A.size() && j<B.size()){
+            
+            int lo = max(A[i].front(), B[j].front());
+            int hi = min(A[i].back(), B[j].back());
+            
+            if(lo<=hi){
+                vector<int> aux;
+                ans.push_back({lo,hi});
+            }
+            
+            if(A[i].back() > B[j].back())
+                j++;
+            else
+                i++;
+        }
+        
+        return ans;
+      
+}
