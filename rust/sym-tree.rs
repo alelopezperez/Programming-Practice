5b317ce2bc76534ac30061f361ff7c82e2434f25
ale <malejlopez@hotmail.com>
2023-02-24 01:25:41 -0400

    file change

--- rust/sym-tree.rs
+++ rust/sym-tree.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 pub struct TreeNode {
     pub val: i32,
     pub left: Option<Rc<RefCell<TreeNode>>>,
@@ -17,6 +19,24 @@ impl TreeNode {
 
 fn main() {}
 
+pub fn helper(left: &Option<Rc<RefCell<TreeNode>>>, right: &Option<Rc<RefCell<TreeNode>>>) -> bool {
+    match (left, right) {
+        (Some(l), Some(r)) => {
+            if l.borrow().val == r.borrow().val {
+                println!("sad");
+                helper(&l.borrow().left, &r.borrow().right)
+                    && helper(&l.borrow().right, &r.borrow().left)
+            } else {
+                false
+            }
+        }
+        (None, None) => true,
+        _ => false,
+    }
+}
 pub fn is_symmetric(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
     let r = root.unwrap();
+    let ans = helper(&r.borrow().left, &r.borrow().right);
+    ans
 }
+
