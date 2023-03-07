7c9fbf417139d080a9bbf9c96a0033df63cb1251
ale <malejlopez@hotmail.com>
2023-03-07 03:29:13 -0400

    Final question in this git proj

--- rust/balanced-binary-tree.rs
+++ rust/balanced-binary-tree.rs
@@ -1,56 +1,54 @@
 // Definition for a binary tree node.
-// #[derive(Debug, PartialEq, Eq)]
-// pub struct TreeNode {
-//   pub val: i32,
-//   pub left: Option<Rc<RefCell<TreeNode>>>,
-//   pub right: Option<Rc<RefCell<TreeNode>>>,
-// }
-// 
-// impl TreeNode {
-//   #[inline]
-//   pub fn new(val: i32) -> Self {
-//     TreeNode {
-//       val,
-//       left: None,
-//       right: None
-//     }
-//   }
-// }
-use std::rc::Rc;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
 use std::cell::RefCell;
 use std::cmp;
+use std::rc::Rc;
+
+fn main() {}
 impl Solution {
-    pub fn counter(root: &Option<Rc<RefCell<TreeNode>>>, mut curr: i32) -> i32{
-        
-        match root{
-            Some(r)=>{
-                curr = cmp::max(Self::counter(&r.borrow().left, curr+1),
-                Self::counter(&r.borrow().right, curr+1));
+    pub fn counter(root: &Option<Rc<RefCell<TreeNode>>>, mut curr: i32) -> i32 {
+        match root {
+            Some(r) => {
+                curr = cmp::max(
+                    Self::counter(&r.borrow().left, curr + 1),
+                    Self::counter(&r.borrow().right, curr + 1),
+                );
 
                 curr
             }
-            None =>{
-                curr-1
-            }
+            None => curr - 1,
         }
     }
     pub fn is_balanced(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-
-        match root{
+        match root {
             Some(r) => {
                 let curr = 1;
-                let ri = Self::counter(&r.borrow().left, curr+1);
-                let l = Self::counter(&r.borrow().right, curr+1);
-                if (ri-l).abs() >1 {
+                let ri = Self::counter(&r.borrow().left, curr + 1);
+                let l = Self::counter(&r.borrow().right, curr + 1);
+                if (ri - l).abs() > 1 {
                     false
-                }
-                else{
-                    Self::is_balanced(r.borrow().left.clone()) && Self::is_balanced(r.borrow().right.clone())
+                } else {
+                    Self::is_balanced(r.borrow().left.clone())
+                        && Self::is_balanced(r.borrow().right.clone())
                 }
             }
-            None =>{
-                true
-            }
+            None => true,
         }
     }
 }
